9aad2dcbbe3ea8eefa3cd6c1cf1f7efc11a377a2
Yaroslav Halchenko <debian@onerussian.com>
2019-03-07 17:10:09 -0500

    RF: tests redone in bats
    bats is for bash, so we would not be able to easily check operation
    in other bash compliant shells... in principle might be not a show
    stopper since at least the syntax is verified and the script is ATM
    tiny.

--- tests/test_basic.sh
+++ tests/test_basic.sh
@@ -1,27 +1,28 @@
-#!/bin/sh
+#!/usr/bin/env bats
 
 # export PATH=$(dirname $0/..):$PATH
 REPROSEED_CMD="../reproseed.sh"
 
-testSource()
-{
+common_checks() {
+	# Common checks to see if all seeds in the environment are equal
+	# to the desired value
+	[ "$REPROSEED" = "$1" ]
+	[ "$AFNI_RANDOM_SEEDVAL" = "$REPROSEED" ]
+	[ "$MVPA_SEED" = "$REPROSEED" ]
+
+}
+
+@test "source basic functionality test" {
 	. $REPROSEED_CMD
-	assertEquals "$AFNI_RANDOM_SEEDVAL" "$REPROSEED"
-	assertEquals "$MVPA_SEED" "$REPROSEED"
+	common_checks "$REPROSEED"
 }
 
-testSourceOutput() {
-	out=`. $REPROSEED_CMD`
-	#assertTrue "echo $out | grep -q '(random)'"
-	assertContains '(random)' "$out"
+@test "source check output" {
+ 	out=`. $REPROSEED_CMD`
+ 	echo $out | grep -q '(random)'
 }
 
-testSourceSeeded()
-{
-	REPROSEED=1 . $REPROSEED_CMD
-	assertEquals "$REPROSEED" 1
-	assertEquals "$AFNI_RANDOM_SEEDVAL" "$REPROSEED"
-	assertEquals "$MVPA_SEED" "$REPROSEED"
+@test "seed and source" {
+ 	REPROSEED=1 . $REPROSEED_CMD
+	eval common_checks 1
 }
-
-. shunit2
